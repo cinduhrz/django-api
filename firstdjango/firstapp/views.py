7aa0ce1c8433d9f3102a158aa252e4a66fc333bc
Cindy Wong <cindywongdev@gmail.com>
2023-02-11 12:37:06 -0500

    initial commit

---
+++ firstdjango/firstapp/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+from django.views import View
+# Create your views here.
+
+# class-based view
+class FirstView(View):
+    # this function will run for a get request
+    # functions should be named after their restful route verbs
+    def get(self, request):
+        return JsonResponse({"hello": "world-get"})
+    
+    def post(self, request):
+        return JsonResponse({"hello": "world-post"})
+    
+    def put(self, request):
+        return JsonResponse({"hello": "world-put"})
+    
+    def delete(self, request):
+        return JsonResponse({"hello": "world-delete"})
+    
+    
+# function-based view
+def functionview(request):
+    return JsonResponse({"hey": "it works"})
+
+def templateview(request):
+    return render(request, "cheese.html", {"hello": "hello world"})
